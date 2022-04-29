36d68996916f7dadac65597533771ddffd85f452
Alex Sahm <erebos12@web.de>
2022-04-29 20:55:25 +0200

    some refactoring

--- java/src/main/java/utils/SlidingWindow.java
+++ java/src/main/java/utils/SlidingWindow.java
@@ -17,31 +17,28 @@ public class SlidingWindow {
 
     public static int maxSumOfKElementsInArray(List<Integer> list, int k) throws Exception {
         if (k > list.size()) {
-            return -1;
+            throw new Exception("Invalid window size!");
         }
-        int max_sum = 0;
+        int maxSum = 0;
         int lower = 0;
         int upper = k - 1;
         while (upper < list.size()) {
-            int window_sum = sumFromLowerToUpperIndex(list, lower, upper);
-            max_sum = Math.max(window_sum, max_sum);
+            int windowSum = sumFromLowerToUpperIndex(list, lower, upper);
+            maxSum = Math.max(windowSum, maxSum);
             lower++;
             upper++;
         }
-        return max_sum;
+        return maxSum;
     }
 
-    public static int maxSumOfKElementsInArray(int arr[], int n, int k) {
-        int max_sum = Integer.MIN_VALUE;
+    public static int maxSumOfKElementsInArray(int[] arr, int n, int k) {
+        int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < n - k + 1; i++) {
-            int current_sum = 0;
+            int currentSum = 0;
             for (int j = 0; j < k; j++)
-                current_sum = current_sum + arr[i + j];
-
-            // Update result if required.
-            max_sum = Math.max(current_sum, max_sum);
+                currentSum = currentSum + arr[i + j];
+            maxSum = Math.max(currentSum, maxSum);
         }
-
-        return max_sum;
+        return maxSum;
     }
 }
