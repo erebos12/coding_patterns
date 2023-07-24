78197bdf16f6bed88324a6c5cd8ce2fa87851692
Alex S <erebos12@web.de>
2023-07-24 15:40:55 +0200

    invertPermutation()

--- java/src/main/java/arrays/SignumPermutation.java
+++ java/src/main/java/arrays/SignumPermutation.java
@@ -14,4 +14,19 @@ public class SignumPermutation {
         int signum = (errorCount % 2 == 0) ? 1 : -1;
         return new SignumResult(signum, errorCount);
     }
+
+    /*
+       Inverting permutation: Index becomes value and value becomes index
+       ATTENTION:
+         Values in array always starts with 1,
+         so 1 corresponds then to index = 0, 2 to index = 1 and so on.
+         That means (value - 1) would then be the new index.
+     */
+    public static int[] invertPermutation(int[] array) {
+        int[] inverted = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            inverted[array[i] - 1] = i + 1;
+        }
+        return inverted;
+    }
 }
--- java/src/test/java/array/SignumPermutationTest.java
+++ java/src/test/java/array/SignumPermutationTest.java
@@ -3,7 +3,9 @@ package array;
 import arrays.SignumResult;
 import org.junit.jupiter.api.Test;
 
+import static arrays.SignumPermutation.invertPermutation;
 import static arrays.SignumPermutation.signumOfPermutation;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SignumPermutationTest {
@@ -41,5 +43,32 @@ public class SignumPermutationTest {
         assertEquals(1, res.signum);
         assertEquals(0, res.errorCount);
 
+    }
+
+    @Test
+    void testInvertPermutation() {
+        int[] res = invertPermutation(new int[]{3, 1, 4, 5, 2});
+        assertArrayEquals(new int[]{2, 5, 1, 3, 4}, res);
+
+        res = invertPermutation(new int[]{3, 4, 1, 2, 5});
+        assertArrayEquals(new int[]{3, 4, 1, 2, 5}, res);
+
+        res = invertPermutation(new int[]{3, 1, 2});
+        assertArrayEquals(new int[]{2, 3, 1}, res);
+
+        res = invertPermutation(new int[]{2, 1, 3});
+        assertArrayEquals(new int[]{2, 1, 3}, res);
+
+        res = invertPermutation(new int[]{1, 2, 3});
+        assertArrayEquals(new int[]{1, 2, 3}, res);
+
+        res = invertPermutation(new int[]{2, 1});
+        assertArrayEquals(new int[]{1, 2}, res);
+
+        res = invertPermutation(new int[]{1, 2});
+        assertArrayEquals(new int[]{1, 2}, res);
+
+        res = invertPermutation(new int[]{1});
+        assertArrayEquals(new int[]{1}, res);
     }
 }
