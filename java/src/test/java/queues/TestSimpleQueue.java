0fe7df96f8dad8cd2c7117f4f7b94ed891dcd025
Alex S <erebos12@web.de>
2023-08-08 12:58:35 +0200

    queue - round robin via queue

--- java/src/test/java/queues/TestSimpleQueue.java
+++ java/src/test/java/queues/TestSimpleQueue.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TestSimpleQueue {
 
@@ -24,5 +25,22 @@ public class TestSimpleQueue {
 
         // Printing the top element again
         System.out.println("Remaining: " + pQueue);
+    }
+
+    @Test
+    void roundRobinViaQueue() {
+        Queue<String> queue = new ConcurrentLinkedQueue<>();// FIFO
+        queue.add("127.0.0.1");
+        queue.add("127.0.0.2");
+        queue.add("127.0.0.3");
+        for (int i = 0; i < 30; i++) {
+            System.out.println(serverByRoundRobin(queue));
+        }
+    }
+
+    private String serverByRoundRobin(Queue<String> queue) {
+        var top = queue.poll(); // remove next from queue
+        queue.add(top); // enqueue it again to the queue
+        return top;
     }
 }
