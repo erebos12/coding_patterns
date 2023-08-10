d9872e824001111a53200db52f7daa88d3f8f5a8
Alex S <erebos12@web.de>
2023-08-10 09:05:43 +0200

    comment on PriorityQueue

--- java/src/main/java/heaps/TwoHeapsCapitalProblem.java
+++ java/src/main/java/heaps/TwoHeapsCapitalProblem.java
@@ -7,9 +7,13 @@ public class TwoHeapsCapitalProblem {
 
     public static int maximumCapital(int c, int k, int[] capitals, int[] profits) {
         int currentCapital = c;
+        /*
+         The priority queue data structure in Java can be directly used to represent the min-heap.
+         By default, the priority queue implements min-heap.
+         */
         PriorityQueue<int[]> capitalMinHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         for (int i = 0; i < capitals.length; ++i) {
-            capitalMinHeap.offer(new int[] {capitals[i], i});
+            capitalMinHeap.offer(new int[]{capitals[i], i});
         }
         PriorityQueue<int[]> profitsMaxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         int i = 0;
