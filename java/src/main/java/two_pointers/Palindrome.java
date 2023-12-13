10793920b2c40b4067c3600a48bb56b733490031
Alex Sahm <alexander.sahm@aleri.de>
2023-12-13 10:17:40 +0100

    slight refactoring of isPalindrome()

--- java/src/main/java/two_pointers/Palindrome.java
+++ java/src/main/java/two_pointers/Palindrome.java
@@ -2,25 +2,22 @@ package two_pointers;
 
 public class Palindrome {
 
-    // Time complexity: O(n) + O(1/2*n) ~ O(n)
+    // Time complexity: O(1/2*n) ~ O(n)
     public static boolean isPalindrome(String s) {
-        char[] chars = s.toCharArray(); // O(n)
-        int nbrSteps = 0;
         int start = 0;
-        int end = chars.length - 1;
-        while (start < end) { // O(n/2)
-            nbrSteps++;
-            String charLeft = String.valueOf(chars[start]);
-            String charRight = String.valueOf(chars[end]);
-            if (!charLeft.equalsIgnoreCase(charRight)) {
+        int end = s.length() - 1;
+        while (start < end) {
+            char charLeft = s.charAt(start);
+            char charRight = s.charAt(end);
+            if (Character.toLowerCase(charLeft) != Character.toLowerCase(charRight)) {
                 return false;
             }
             start++;
             end--;
         }
-        System.out.println(nbrSteps + " steps for " + s.length() + " elements");
         return true;
     }
+
 
     // Time complexity: 4*O(n) = O(4n) ~ O(n)
     public static boolean isPalindrome2(String s) {
